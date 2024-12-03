00e9441759fc1bfba382c911abcb8b36a556bfad
remuslum <57741494+remuslum@users.noreply.github.com>
2024-12-03 17:49:56 +0800

    First commit

---
+++ src/main/java/sg/nus/edu/iss/vttp5a_ssf_miniproject/Vttp5aSsfMiniprojectApplication.java
@@ -0,0 +1,13 @@
+package sg.nus.edu.iss.vttp5a_ssf_miniproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Vttp5aSsfMiniprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Vttp5aSsfMiniprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sg/nus/edu/iss/vttp5a_ssf_miniproject/Vttp5aSsfMiniprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.nus.edu.iss.vttp5a_ssf_miniproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Vttp5aSsfMiniprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
